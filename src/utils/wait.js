377ad44c91b9ac74705c969449e0b64f35ddbd6d
brian kirkby <bkirkby@instructure.com>
2019-12-03 17:03:53 -0700

    remove waits

--- src/utils/wait.js
+++
@@ -1,9 +0,0 @@
-// This function just simulates an async operation like an API call.
-export default function(seconds, cb) {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve("hurray")
-			cb("hurray")
-		}, seconds * 1000)
-	})
-}
